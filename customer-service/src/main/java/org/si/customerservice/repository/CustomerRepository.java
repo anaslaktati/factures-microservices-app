b873262ce96368e1d59b0a14dbc0a89040c3e238
Anas LAKTATI <anas.laktati@atos.net>
2026-06-03 09:57:51 +0100

    first part

---
+++ customer-service/src/main/java/org/si/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.si.customerservice.repository;
+
+import org.si.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
